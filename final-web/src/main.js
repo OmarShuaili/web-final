bf8de663818500fe94ecdecf8db2f14fa5c692aa
OmarShuaili <67199849+OmarShuaili@users.noreply.github.com>
2020-12-21 22:51:43 +0000

    Add files via upload

---
+++ final-web/src/main.js
@@ -0,0 +1,151 @@
+document.getElementById("log-in").addEventListener("click", login, false);
+let nCount = selector => {
+    $(selector).each(function () {
+        $(this)
+            .animate({
+                Counter: $(this).text()
+            }, {
+                // A string or number determining how long the animation will run.
+                duration: 4000,
+                // A string indicating which easing function to use for the transition.
+                easing: "swing",
+                /**
+                 * A function to be called for each animated property of each animated element. 
+                 * This function provides an opportunity to
+                 *  modify the Tween object to change the value of the property before it is set.
+                 */
+                step: function (value) {
+                    $(this).text(Math.ceil(value));
+                }
+            });
+    });
+};
+
+let a = 0;
+$(window).scroll(function () {
+    // The .offset() method allows us to retrieve the current position of an element  relative to the document
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount(".rect > h1");
+    }
+});
+
+
+
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+    // get the complete hight of window
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+});
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe');
+const gradients = document.querySelectorAll('.gradient');
+const shoeBg = document.querySelector('.shoeBackground');
+
+let prevColor = "blue";
+let animationEnd = true;
+
+function changeSize() {
+    sizes.forEach(size => size.classList.remove('active'));
+    this.classList.add('active');
+}
+
+function changeColor() {
+    if (!animationEnd) return;
+    let primary = this.getAttribute('primary');
+    let color = this.getAttribute('color');
+    let shoe = document.querySelector(`.shoe[color="${color}"]`);
+    let gradient = document.querySelector(`.gradient[color="${color}"]`);
+    let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
+
+    if (color == prevColor) return;
+
+    colors.forEach(c => c.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary', primary);
+
+    shoes.forEach(s => s.classList.remove('show'));
+    shoe.classList.add('show');
+
+    gradients.forEach(g => g.classList.remove('first', 'second'));
+    gradient.classList.add('first');
+    prevGradient.classList.add('second');
+
+    prevColor = color;
+    animationEnd = false;
+
+    gradient.addEventListener('animationend', () => {
+        animationEnd = true;
+    })
+}
+
+
+function is_creditCard(x)
+{
+    var x = document.getElementById("cnum");
+ regexp = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
+  
+        if (regexp.test(x))
+          {
+            return true;
+          }
+        else
+          {
+            return false;
+          }
+}
+
